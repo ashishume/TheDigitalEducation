b16c13e5f5cd90da9866d9f794165faa52dbe387
Ashish Debnath <ashishume@gmail.com>
2020-04-27 19:18:05 +0530

    Keystore added

--- src/App.js
+++ src/App.js
@@ -2,26 +2,14 @@ import React, {Component, Fragment} from 'react';
 import BannerAd from './components/Ads/BannerAds';
 import MainRouting from './Routes/MainRouting';
 import LoaderComponent from './components/LoaderComponent';
-// import messaging from '@react-native-firebase/messaging';
 
 class App extends Component {
-  // componentDidMount() {
-  //   this.checkPermission();
-  // }
-
-  // async checkPermission() {
-  //   const settings = await messaging().requestPermission();
-  //   if (settings) {
-  //     console.log('Permission settings:', settings);
-  //   }
-  // }
-
   render() {
     return (
       <Fragment>
         <LoaderComponent />
         <MainRouting />
-        {/* <BannerAd /> */}
+        <BannerAd />
       </Fragment>
     );
   }
